9693bf3707fc42c4dae4c1b5fc41618852ab2225
tannerbk <tannerbk@berkeley.edu>
2022-05-23 12:25:04 -0700

    update pmt database

--- pmt_testing_database/pmt.sql
+++ pmt_testing_database/pmt.sql
@@ -1,19 +1,22 @@
 CREATE TABLE pmt_information(
-    key                     bigserial PRIMARY KEY,
-    timestamp               timestamp with time zone default now(),
-    source                  text, -- NOT NULL
-    pmt_type                text, -- NOT NULL
-    pmt_id                  text, -- NOT NULL
-    high_voltage            smallint, -- NOT NULL
-    tts_sigma               real,
-    late_pulsing_pct        real,
-    after_pulsing_pct       real,
-    pre_pulsing_pct         real,
-    dark_rate               real,
-    charge_peak             real,
-    charge_width            real,
-    charge_peak_to_valley   real,
-    high_charge_pct         real
-    -- entries              bigint
-    -- magnetic compensation real
+    key                      bigserial PRIMARY KEY,
+    timestamp                timestamp with time zone default now(),
+    entries                  bigint NOT NULL,
+    source                   text NOT NULL,
+    pmt_type                 text NOT NULL,
+    pmt_id                   text NOT NULL,
+    high_voltage             smallint NOT NULL,
+    tts_sigma                real,
+    late_pulsing_pct         real,
+    after_pulsing_pct        real,
+    pre_pulsing_pct          real,
+    dark_rate                real,
+    charge_peak              real,
+    charge_width             real,
+    charge_peak_to_valley    real,
+    high_charge_pct          real,
+    threshold                real,
+    coincidence_rate         real,     
+    magnetic_compensation    text,
+    --settling_time            text,
 )
