6885967d989e74aed5599c85764c9379311a91f4
Mackenzie Duce <mduce@nino.physics.berkeley.edu>
2022-06-15 16:06:47 -0700

    Added ability to plot both charge and deltat comparison histograms for four datasets

--- plotting_code/multianalysis_v1.py
+++ plotting_code/multianalysis_v1.py
@@ -1,7 +1,9 @@
-# this is a test python code developed by mduce to learn how to use pyroot
-# the import file to the code simply must be a .root
-# for now, i've dumped ZT0076_1650V_Neck_and_Cone_lappd_0_gr0_ch1.root in .
-# currently, this is for TWO DATASETS
+# this is python code was developed by mduce so multiple pmt datasets can be
+# compared after they have been run through analysis.py
+# input: four .root files outputted from analysis.py
+# output: layered histogram of four datasets for desired parameter
+# 
+# NOTE: currently, this is for 4 datasets and is designed to look at deltat
 
 import ROOT # import the CERN ROOT module
 import sys
@@ -41,11 +43,9 @@ def open_root(fi1):
 	ht1 = open_tree(tree1)
 	return ht1
 
-def normalize(data):
+def normalize(data, crop_min, crop_max, bw):
 	# crop dataset so that all datasets can be plotted together
 	# limit deltat values to those between 0 and X ns
-	crop_min = 10
-	crop_max = 20
 	y1 = [x for x in data if x>=crop_min and x<=crop_max]
 	# slight variations in the datasets mean t doesnt exactly
 	# equal zero anywhere, so we'll force the min and maxes
@@ -57,7 +57,7 @@ def normalize(data):
 
 	# declare desired bin width and build array
 	# note +val and ,val must be equal
-	b = np.arange(float(min(y1)),float(max(y1)) + .2, .2)
+	b = np.arange(float(min(y1)),float(max(y1)) + bw, bw)
 
 	# build histogram of data (y1) and desired bins
 	bin_height, bin_boundary = np.histogram(y1,bins=len(b))
@@ -75,35 +75,50 @@ def normalize(data):
 
 	return bin_boundary, bin_height, width
 
-def plot(y1, y2, y3, y4):
-	f1 = str(input("Input legend value for dataset 1:\n"))
-	f2 = str(input("Input legend value for dataset 2:\n"))
-	f3 = str(input("Input legend value for dataset 3:\n"))
-	f4 = str(input("Input legend value for dataset 4:\n"))	
+def plot(t1, t2, t3, t4, c1, c2, c3, c4):
+	f1 = str(input("Input HV for dataset 1:[numerical only] \n"))
+	f2 = str(input("Input HV for dataset 2 [numerical only]:\n"))
+	f3 = str(input("Input HV for dataset 3 [numerical only]:\n"))
+	f4 = str(input("Input HV for dataset 4 [numerical only]:\n"))	
 
-	bb1, bh1, bw1 = normalize(y1)
-	bb2, bh2, bw2 = normalize(y2)
-	bb3, bh3, bw3 = normalize(y3)
-	bb4, bh4, bw4 = normalize(y4)
+	tbb1, tbh1, tbw1 = normalize(t1,10, 20, .1)
+	tbb2, tbh2, tbw2 = normalize(t2,10, 20, .1)
+	tbb3, tbh3, tbw3 = normalize(t3,10, 20, .1)
+	tbb4, tbh4, tbw4 = normalize(t4,10, 20, .1)
+
+	cbb1, cbh1, cbw1 = normalize(c1,0, 6, .1)
+	cbb2, cbh2, cbw2 = normalize(c2,0, 6, .1)
+	cbb3, cbh3, cbw3 = normalize(c3,0, 6, .1)
+	cbb4, cbh4, cbw4 = normalize(c4,0, 6, .1)
 	
 	plt.figure(1)
-	plt.step(bb1[:-1],bh1,'k',linestyle='-',linewidth=2,where='mid')
-	plt.bar(bb1[:-1],bh1,width=bw1,linewidth=0,alpha=0.3)
-	plt.step(bb2[:-1],bh2,'k',linestyle='--',linewidth=2,where='mid')
-	plt.bar(bb2[:-1],bh2,width=bw2,linewidth=0,alpha=0.3)
-	plt.step(bb3[:-1],bh3,'k',linestyle=':',linewidth=2,where='mid')
-	plt.bar(bb3[:-1],bh3,width=bw3,linewidth=0,alpha=0.3)
-	plt.step(bb4[:-1],bh4,'k',linestyle='-.',linewidth=2,where='mid')
-	plt.bar(bb4[:-1],bh4,width=bw4,linewidth=0,alpha=0.3)
+	plt.step(tbb1[:-1],tbh1,'k',linestyle='-',linewidth=1,where='mid')
+	plt.bar(tbb1[:-1],tbh1,width=tbw1,linewidth=0,alpha=0.3)
+	plt.step(tbb2[:-1],tbh2,'k',linestyle='--',linewidth=1,where='mid')
+	plt.bar(tbb2[:-1],tbh2,width=tbw2,linewidth=0,alpha=0.3)
+	plt.step(tbb3[:-1],tbh3,'k',linestyle=':',linewidth=1,where='mid')
+	plt.bar(tbb3[:-1],tbh3,width=tbw3,linewidth=0,alpha=0.3)
+	plt.step(tbb4[:-1],tbh4,'k',linestyle='-.',linewidth=1,where='mid')
+	plt.bar(tbb4[:-1],tbh4,width=tbw4,linewidth=0,alpha=0.3)
 
-	#plt.bar(bb1[:-1], bh1, width = bw1)
-	#plt.bar(bb2[:-1], bh2, width = bw2)
-	#plt.bar(bb3[:-1], bh3, width = bw4)
-	#plt.bar(bb4[:-1], bh4, width = bw4)
-	
 	plt.xlabel("Time [ns]")
 	plt.ylabel("Relative Intensity")
 	plt.legend([f1, f2, f3, f4])
+
+	plt.figure(2)
+	plt.step(cbb1[:-1],cbh1,'k',linestyle='-',linewidth=1,where='mid')
+	plt.bar(cbb1[:-1],cbh1,width=cbw1,linewidth=0,alpha=0.3)
+	plt.step(cbb2[:-1],cbh2,'k',linestyle='--',linewidth=1,where='mid')
+	plt.bar(cbb2[:-1],cbh2,width=cbw2,linewidth=0,alpha=0.3)
+	plt.step(cbb3[:-1],cbh3,'k',linestyle=':',linewidth=1,where='mid')
+	plt.bar(cbb3[:-1],cbh3,width=cbw3,linewidth=0,alpha=0.3)
+	plt.step(cbb4[:-1],cbh4,'k',linestyle='-.',linewidth=1,where='mid')
+	plt.bar(cbb4[:-1],cbh4,width=cbw4,linewidth=0,alpha=0.3)
+
+	plt.xlabel("Charge [pC]")
+	plt.ylabel("Relative Intensity")
+	plt.legend([f1, f2, f3, f4])
+
 	plt.show()
 
 def plotnormed(y1):
@@ -129,5 +144,17 @@ if __name__=='__main__':
 	dt2, ch2 = (open_root(sys.argv[2]))
 	dt3, ch3 = (open_root(sys.argv[3]))
 	dt4, ch4 = (open_root(sys.argv[4]))
-	plot(dt1, dt2, dt3, dt4)
-	#plotnormed(dt1)
+
+	plot(dt1, dt2, dt3, dt4, ch1, ch2, ch3, ch4)
+
+	#prompt = raw_input("dt or charge?:").lower()
+
+	#if prompt == "dt":
+	#	print "executing dt analysis"
+	#	plot_t(dt1, dt2, dt3, dt4)
+	#elif prompt == "charge":
+	#	print "executing charge analysis"
+	#	#plot(ch1, ch2, ch3, ch4)
+	#else:
+	#	print "improper input, exiting..."
+	#	sys.exit()
