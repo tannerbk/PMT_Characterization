e4063cc207ec400102482c7c33dd329ed3b44ab3
PMT tester <tannerbk@berkeley.edu>
2023-06-08 12:15:47 -0700

    improve example

--- pmt_testing_database/example_connect_to_db.py
+++ pmt_testing_database/example_connect_to_db.py
@@ -1,7 +1,9 @@
 import sqlalchemy
 import sys
 
-def connect():
+if __name__=='__main__':
+
+    key = int(sys.argv[1])
 
     engine = sqlalchemy.create_engine('postgresql://%s:%s@%s:%i/%s' %
                                       ('postgres', 'b33feroni',
@@ -9,27 +11,18 @@ def connect():
                                        pool_recycle=3600)
 
     conn = engine.connect()
-    return conn
 
-
-def select_info(conn, key):
-
-    command = ('SELECT tts_sigma, dark_rate FROM pmt_information WHERE key=%d' % (key,))
+    command = ('SELECT * FROM pmt_information WHERE key=%d' % key)
 
     result = conn.execute(command)
-    row = result.fetchall()
+    row = result.fetchone()
+    keys = result.keys()
 
-    for tts_sigma, dark_rate in row:
-        print "TTS (sigma):", tts_sigma
-        print "TTS (FWHM):", tts_sigma*2.335
-        print "Dark-rate (Hz):", dark_rate
+    data = dict(zip(row.keys(), row))
 
+    print("{:<25} {:<10}".format("Name","Value"))
+    print ("------------")
+    for key in data:
+        print("{:<25} {:<10}".format(key,data[key]))
 
-if __name__=='__main__':
-
-    key = int(sys.argv[1])
-
-    conn = connect()
-
-    select_info(conn, key)
-
+    print ("------------")
