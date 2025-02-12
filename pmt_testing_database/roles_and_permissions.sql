4ce8ce9ee2ac162bfab29fc04352ff5099768c8a
tannerbk <tannerbk@berkeley.edu>
2025-02-12 13:40:55 -0800

    update roles

--- pmt_testing_database/roles_and_permissions.sql
+++ pmt_testing_database/roles_and_permissions.sql
@@ -1,3 +1,3 @@
-GRANT SELECT, INSERT ON pmt_information TO db_user;
+GRANT SELECT, INSERT ON pmt_information, hv_status TO db_user;
 
-GRANT SELECT, USAGE ON SEQUENCE pmt_information_key_seq TO db_user;
+GRANT SELECT, USAGE ON SEQUENCE pmt_information_key_seq, hv_status_key_seq TO db_user;
